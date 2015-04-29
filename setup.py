c0c30d483667a7044b9a541c85dd3df9b23770f6
zachasme <zachasme@gmail.com>
2015-04-29 20:26:24 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='statbank',
-    version='0.1',
+    version='0.2',
     description='Statbank API client library',
     url='http://github.com/gisgroup/statbank-python',
     author='Gis Group ApS',
